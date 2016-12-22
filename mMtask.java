d7033682d3522a8eadbd555ce2100101ad200653
Snail2037 <sahilsinha2000@yahoo.com>
2016-12-22 17:05:23 -0500

    Update mMtask.java
    Takes an input text file and converts all the m's to M's and all the M's to m's.

--- mMtask.java
+++ mMtask.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class mMtask {
 	
 	public static void main(String[] args) {
-		String fileName = "KLSadd.tex";
+		String fileName = "KLSadd.tex"; //specified file to convert
 
         try {
             FileReader inputFile = 
@@ -13,22 +13,22 @@ public class mMtask {
 
             BufferedReader br = 
                 new BufferedReader(inputFile);
-            PrintWriter outputFile = (new PrintWriter(new FileWriter("mMoutput.txt")));
+            PrintWriter outputFile = (new PrintWriter(new FileWriter("mMoutput.txt"))); //output file setup
             int x;
             Character readLetter;
             
-            while((x = br.read()) != -1) {
+            while((x = br.read()) != -1) {  //reading character by character until the end of the stream has been reached (-1)
             	readLetter = Character.valueOf((char) x);
-            	if ((readLetter == 'm') || (readLetter == 'M')) {   	
+            	if ((readLetter == 'm') || (readLetter == 'M')) {   //lowercase or uppercase conditionals
             	if (Character.isUpperCase(readLetter)) 
             		outputFile.write(Character.toLowerCase(readLetter));
             	else outputFile.write(Character.toUpperCase(readLetter));
             		}
             	else outputFile.write(readLetter);
-            	}   //while
+            	}   
 
             br.close();     
-            outputFile.close();
+            outputFile.close(); //closes both files
         }
         catch(FileNotFoundException ex) {
             System.out.println(
