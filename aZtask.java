61a84d769f621cb1546583275c62e61ecc752fe8
Snail2037 <sahilsinha2000@yahoo.com>
2016-12-22 16:59:00 -0500

    Update aZtask.java
    Converts letters a-z to A-Z and A-Z to a-z for the same input  used for the mM task.

--- aZtask.java
+++ aZtask.java
@@ -5,28 +5,28 @@ import java.util.*;
 public class aZtask {
 	
 	public static void main(String[] args) {
-		String fileName = "KLSadd.tex";
+		String fileName = "KLSadd.tex"; //specified file to convert
 
         try {
             FileReader inputFile = 
                 new FileReader(fileName);
 
-            BufferedReader br = 
+            BufferedReader br = //buffers input from the file
                 new BufferedReader(inputFile);
-            PrintWriter outputFile = (new PrintWriter(new FileWriter("aZoutput.txt")));
+            PrintWriter outputFile = (new PrintWriter(new FileWriter("aZoutput.txt"))); //sets up the output file
             int x;
             Character readLetter;
             
             while((x = br.read()) != -1) {
             	readLetter = Character.valueOf((char) x);
-            	if (Character.isUpperCase(readLetter)) 
+            	if (Character.isUpperCase(readLetter))     //if M, print m
             		outputFile.write(Character.toLowerCase(readLetter));
-            	else outputFile.write(Character.toUpperCase(readLetter));
+            	else outputFile.write(Character.toUpperCase(readLetter)); //vice versa
             	
             	}   
 
             br.close();     
-            outputFile.close();
+            outputFile.close(); //closes files!
         }
         catch(FileNotFoundException ex) {
             System.out.println(
